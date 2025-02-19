d97ccfa9f0f5315769baedb8ad2261e0e5fcda40
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-19 11:51:56 -0500

    Creating the models

--- shop_project/store_app/models.py
+++ shop_project/store_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+    
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    address = models.CharField(max_length=256)
+    contact = models.CharField(max_length=10)
+
+    @staticmethod
+    def get_customer_by_email(email): 
+        try: 
+            return Customer.objects.get(email=email) 
+        except: 
+            return False
+  
+    def isExists(self): 
+        if Customer.objects.filter(email=self.email): 
+            return True
+  
+        return False
+    
+# Create a Products Model
+
+# Create a Orders Model
