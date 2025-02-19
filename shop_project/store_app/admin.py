cd98f642f2d8ad762398a6da7fbf62172d134a27
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-19 15:26:12 -0500

    Created categories in admin

--- shop_project/store_app/admin.py
+++ shop_project/store_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from store_app.models import UserProfileInfo
+from store_app.models import UserProfileInfo, Category, Customer, Products, Order
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Products)
+admin.site.register(Order)
