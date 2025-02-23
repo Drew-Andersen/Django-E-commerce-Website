5e170df5e6168ea2ccb3ff00474a59201a95f1ea
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-23 08:43:01 -0500

    Added the start of the cart.html page

--- shop_project/store_app/forms.py
+++ shop_project/store_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
+# Work on editing the form to not show the restraints on the password field until it is clicked
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
